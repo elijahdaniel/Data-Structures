210ac3795c10af76a6368401493d943e2204422a
Elijah Paminsan <e.paminsan@gmail.com>
2020-05-21 11:23:04 -0700

    tims gp notes

---
+++ guided_project/bigOnotation.py
@@ -0,0 +1,15 @@
+
+# tim's notes
+def find_name(name, phone_book):
+    a = 2
+    b = 10**a
+    c = 3 + 4
+    for person in phone_book:
+        if name == person:
+            return True
+
+
+name = 'kim'
+phone_book = ['tim', 'dan', 'oliver', 'kim']
+
+O(1 + 1 + 4)
